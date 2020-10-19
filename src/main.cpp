3270de04ab6a44aefe0c0eff592b4db1aefaa371
Ludvig Halling <ludde.halling@hotmail.com>
2020-10-19 10:55:11 +0200

    Currently adding support for multiple tests in input

--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,14 @@ int main(int argc, char** argv)
 
     auto totalStartTime = std::chrono::steady_clock::now();
     auto startTime = std::chrono::steady_clock::now();
-    std::string file = argv[1];
-    // std::cout << "file: " << file << std::endl;
-    unsigned int size = std::stoul(std::string(argv[2]));
-    // std::cout << "size: " << size << std::endl;
-    std::string dtCase = argv[3];
+    
+    std::string file = argv[1]; //the file that should be tested
+    unsigned int size = std::stoul(std::string(argv[2])); //the amount of nodes
+    std::string dtCase = argv[3]; //timecase
     float beta = stof(std::string(argv[4]));
-    std::string output = argv[5];
-    std::cout << dtCase << std::endl;
+    std::string output = argv[5]; //the file that should get the output
+    std::string testCases = argv[6];
+
     NetworkHandler handler = NetworkHandler(file, size);
     auto endTime = std::chrono::steady_clock::now();
 
@@ -41,30 +41,45 @@ int main(int argc, char** argv)
     {
         seed.push_back(std::stoul(argv[i]));
     }
-    // std::vector<unsigned int> seed = {1000205}; //1000205
-    startTime = std::chrono::steady_clock::now();
+    
+    if (testCases == "single")
+    {
+        startTime = std::chrono::steady_clock::now();
 
-    InformedNodes reachedNodes = icm::diffuseInformation(handler, seed, size, beta, dtCase);
+        InformedNodes reachedNodes = icm::diffuseInformation(handler, seed, size, beta, dtCase);
 
-    endTime = std::chrono::steady_clock::now();
-    time = endTime - startTime;
+        endTime = std::chrono::steady_clock::now();
+        time = endTime - startTime;
 
-    std::cout << "Ran ICM algorithm" << std::endl;
-    std::cout << "took: " << time.count() << " seconds." << std::endl;
+        std::cout << "Ran ICM algorithm" << std::endl;
+        std::cout << "took: " << time.count() << " seconds." << std::endl;
 
-    time = endTime - startTime;
+        time = endTime - startTime;
 
-    if (output == "terminal")
-        reachedNodes.outputNodesToTerminal();
+        if (output == "terminal")
+            reachedNodes.outputNodesToTerminal();
+        else
+            reachedNodes.outputNodesToFile(output);
+
+        std::cout << "reached end of program" << std::endl;
+
+        endTime = std::chrono::steady_clock::now();
+        time = endTime - totalStartTime;
+
+        std::cout << "took in total: " << time.count() << " sec" << std::endl;
+    }
     else
-        reachedNodes.outputNodesToFile(output);
+    {
+        std::ifstream input;
+        input.open(testCases);
+        std::string line;
+        while (std::getline(input, line))
+        {
+            
+        }
 
-    std::cout << "reached end of program" << std::endl;
-
-    endTime = std::chrono::steady_clock::now();
-    time = endTime - totalStartTime;
-
-    std::cout << "took in total: " << time.count() << " sec" << std::endl;
-
+        input.close();
+    }
+    
     return 0;
 }
